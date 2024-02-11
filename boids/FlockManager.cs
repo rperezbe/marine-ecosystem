a88727604cd81cfc2353f1f28d5fae51d23a0e14
Roger Pérez <48314354+rperezbe@users.noreply.github.com>
2024-02-11 18:23:29 +0100

    add some random food spawn and healthbar

--- boids/FlockManager.cs
+++ boids/FlockManager.cs
@@ -6,6 +6,9 @@ public class FlockManager : MonoBehaviour {
 
     public static FlockManager FM;
     public GameObject fishPrefab;
+    public GameObject healthBarPrefab;
+    public GameObject foodPrefab;
+    private float timer = 0.0f;
     public int numFish = 40;
     public GameObject[] allFish;
     public Vector3 swimLimits = new Vector3(2.0f, 2.0f, 2.0f);
@@ -19,6 +22,8 @@ public class FlockManager : MonoBehaviour {
     [Range(0.0f, 5.0f)] public float maxSpeed;
     [Range(1.0f, 10.0f)] public float neighbourDistance;
     [Range(1.0f, 5.0f)] public float rotationSpeed;
+    [Header("Food Settings")]
+    public float spawnInterval = 5.0f;
 
     void Start() {
 
@@ -31,7 +36,13 @@ public class FlockManager : MonoBehaviour {
                 Random.Range(-swimLimits.y, swimLimits.y),
                 Random.Range(-swimLimits.z, swimLimits.z));
 
-            allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+            //instance of the fish
+            GameObject fish = Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i] = fish;
+
+            //instance of the health bar
+            GameObject healthBarInstance = Instantiate(healthBarPrefab, fish.transform);
+            healthBarInstance.transform.localPosition = new Vector3(0f, 0.12f, 0f); //relative position to the fish
         }
 
         FM = this;
@@ -40,9 +51,19 @@ public class FlockManager : MonoBehaviour {
 
 
     void Update() {
-
+        //update the poistion of the food
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval) {
+            timer = 0.0f;
+            //create a new sphere on the random position
+            Vector3 foodPosition = this.transform.position + new Vector3(
+                Random.Range(-swimLimits.x, swimLimits.x),
+                Random.Range(-swimLimits.y, swimLimits.y),
+                Random.Range(-swimLimits.z, swimLimits.z));
+            Instantiate(foodPrefab, foodPosition, Quaternion.identity);
+        }
+        
         if (Random.Range(0, 100) < 10) {
-
             goalPos = this.transform.position + new Vector3(
                 Random.Range(-swimLimits.x, swimLimits.x),
                 Random.Range(-swimLimits.y, swimLimits.y),
