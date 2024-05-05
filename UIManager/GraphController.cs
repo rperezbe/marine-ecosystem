63b66c896e0f19d141e3aff3e095892818f81b21
Roger Pérez <48314354+rperezbe@users.noreply.github.com>
2024-05-05 21:05:40 +0200

    flexible max and min numbers to display correctly the graph without limitations

--- UIManager/GraphController.cs
+++ UIManager/GraphController.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GraphController : MonoBehaviour
 {
     public Image fishBar;
     public Image fishBornBar;
     public Image fishDeadBar;
+    public TextMeshProUGUI maxNumber;
+    public TextMeshProUGUI mediumNumber;
+    
     private float maxFish = 100; //we put this maximum to normalize the value, we can change it
-
     void Update()
     {
         UpdateGraph();
@@ -15,6 +18,11 @@ public class GraphController : MonoBehaviour
 
     void UpdateGraph()
     {
+        //assign the maxNumber text value to the maxFish
+        maxNumber.text = maxFish.ToString();
+        //assign the mediumNumber text value to the half of the maxFish
+        mediumNumber.text = (maxFish / 2).ToString();
+
         //if the FlockManager is not initialized, return
         if(FlockManager.FM == null)
         {
@@ -41,6 +49,12 @@ public class GraphController : MonoBehaviour
 
         //update the fill amount of the fish dead bar
         fishDeadBar.fillAmount = normalizedValueDead;
+
+        //see if any value is greater than the maxFish, then update the maxFish to the double of the current value
+        if(currentFish > maxFish || currentFishBorn > maxFish || currentFishDead > maxFish)
+        {
+            maxFish = maxFish * 2;
+        }
     }
 
 }
