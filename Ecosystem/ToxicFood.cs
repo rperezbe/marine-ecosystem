5e69ff6136ac85b37de88852a4a34c2edc515d48
Roger Pérez <48314354+rperezbe@users.noreply.github.com>
2024-06-01 19:35:52 +0200

    add failing method

--- Ecosystem/ToxicFood.cs
+++ Ecosystem/ToxicFood.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class ToxicFood : Food 
 {
+    public float fallSpeed = 0.5f;
+
     void Start()
     {
         //we can use the singleton pattern of the FlockManager to get the nutrition value of the food
         //if we want to change the nutrition value during the simulation, we can do it on the consume method
         nutritionValue = FlockManager.FM.nutritionValueToxic;
+    }
+
+    //fall each frame
+    void Update()
+    {
+        Fall();
+    }
+
+    //falling method
+    void Fall()
+    {
+        //if the food is at the y axis down 1,2, stop moving
+        if (transform.position.y < -1.2f)
+        {
+            //stop moving
+        }
+        else
+        {
+            //we move the food down slowly
+            transform.Translate(Vector3.down * Time.deltaTime * fallSpeed);
+        }
     }
 
     public override void Consume(Simulation consumer) 
